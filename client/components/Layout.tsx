7cb3756d707e84c06259b9c141ea10dea72435ba
Tristan Bulmer <63182722+Hoffee-Toffee@users.noreply.github.com>
2023-11-16 12:09:42 +1300

    Added Common Header + Footer to Layout Component

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -4,12 +4,49 @@ export default function Layout() {
   return (
     <>
       <header>
-        <h1>HEADER STUFF HERE</h1>
+        <script
+          src="https://kit.fontawesome.com/251912c541.js"
+          crossOrigin="anonymous"
+        ></script>
+        <h1>
+          <a href="/">Tristan Bulmer</a>
+        </h1>
+
+        <input type="checkbox" id="nav-toggle" />
+        <label htmlFor="nav-toggle" id="hamburger">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </label>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="../home/home.html">Home</a>
+            </li>
+            <li>
+              <a href="../interests/interests.html">Interests</a>
+            </li>
+            <li>
+              <a href="../blog/blog.html">Blog</a>
+            </li>
+            <li>
+              <a href="../projects/projects.html">Projects</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+
       <main>
         <Outlet />
       </main>
-      <footer>Copyright &copy; 2023</footer>
+
+      <footer>
+        <span>
+          <a href="https://www.github.com/Hoffee-Toffee">Tristan Bulmer</a>
+          2023
+        </span>
+      </footer>
     </>
   )
 }
