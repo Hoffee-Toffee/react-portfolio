d8b70b5a1c9b04fd27661e7b0fa835b6e785b8e7
Tristan Bulmer <63182722+Hoffee-Toffee@users.noreply.github.com>
2023-11-16 13:13:51 +1300

    All but calendar data is processed, no update implimentation yet

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -4,7 +4,55 @@ import '../styles/home.css'
 import '../styles/blog.css'
 
 function App() {
-  const { data } = useFruits()
+  const { data, isError, isLoading } = useFruits()
+
+  if (!data || isError || isLoading) {
+    return <p>Loading</p>
+  }
+
+  const { count, updated, calendar } = data
+
+  // Started
+  const started = new Date(
+    'December 31, 2022 11:59 PM GMT+1300 (New Zealand Daylight Time',
+  )
+
+  // Updated
+  const updatedDate = new Date(updated)
+
+  // Now
+  const now = new Date()
+
+  // Ms since count start
+  const msAgo = new Date() - started
+
+  // daily
+  // Divide count by difference in ms of current and start, (ms / (1000*60*60*24)) to get as days
+  const daily = count / (msAgo / (1000 * 60 * 60 * 24))
+
+  // interval
+  const intervalRaw = msAgo / count
+  const interval = formatRelativeDate(intervalRaw, 0)
+
+  // thousand
+  const thousandMS = nextMilestone(1, count)
+
+  const thousand = thousandMS / (count / msAgo) - msAgo + updatedDate.getTime()
+
+  // tenThousand
+  const tenThousandMS = nextMilestone(2, count)
+
+  const tenThousand =
+    tenThousandMS / (count / msAgo) - msAgo + updatedDate.getTime()
+
+  // hunThousand
+  const hunThousandMS = nextMilestone(3, count)
+
+  const hunThousand =
+    hunThousandMS / (count / msAgo) - msAgo + updatedDate.getTime()
+
+  // Data processing
+  // let processed = sneezeCalc(data)
 
   return (
     <div id="main">
@@ -12,26 +60,26 @@ function App() {
         <div className="card">
           <h1>
             <span id="holo">
-              <b id="count">???</b>
+              <b id="count">{count.toLocaleString()}</b>
             </span>{' '}
             sneezes
           </h1>
           <p>
             Count Start:{' '}
             <b id="start" className="date">
-              ???
+              {formatFullDate(started)}
             </b>
           </p>
           <p>
-            Last Updated:
+            Last Updated:{' '}
             <b id="updated" className="date">
-              ???
+              {formatFullDate(updatedDate)}
             </b>
           </p>
           <p>
-            Current Time:
+            Current Time:{' '}
             <b id="now" className="date">
-              ???
+              {formatFullDate(now)}
             </b>
           </p>
           <hr />
@@ -102,31 +150,34 @@ function App() {
           <hr />
           <h2>Averages</h2>
           <p>
-            Average sneezes per day: <b id="daily">???</b>
+            Average sneezes per day: <b id="daily">{daily.toLocaleString()}</b>
           </p>
           <p>
             Average time between sneezes:{' '}
             <b id="interval" className="date">
-              ???
+              {interval}
             </b>
           </p>
           <h2>Milestone Estimates</h2>
           <p>
-            4-digit milestone (<b id="thousandMS">???</b> sneezes):
+            4-digit milestone (
+            <b id="thousandMS">{thousandMS.toLocaleString()}</b> sneezes):{' '}
             <b id="thousand" className="date">
-              ???
+              {formatRelativeDate(thousand, updated)}
             </b>
           </p>
           <p>
-            5-digit milestone (<b id="tenThousandMS">???</b> sneezes):
+            5-digit milestone (
+            <b id="tenThousandMS">{tenThousandMS.toLocaleString()}</b> sneezes):{' '}
             <b id="tenThousand" className="date">
-              ???
+              {formatRelativeDate(tenThousand, updated)}
             </b>
           </p>
           <p>
-            6-digit milestone (<b id="hunThousandMS">???</b> sneezes):
+            6-digit milestone (
+            <b id="hunThousandMS">{hunThousandMS.toLocaleString()}</b> sneezes):{' '}
             <b id="hunThousand" className="date">
-              ???
+              {formatRelativeDate(hunThousand, updated)}
             </b>
           </p>
         </div>
@@ -138,15 +189,14 @@ function App() {
           I'm studying at <a href="https://devacademy.co.nz/">Dev Academy</a>,
           New Zealand.
           <br />
-          For my blog for Dev Academy, see
-          <Link to="/blog">here</Link>.
+          For my blog for Dev Academy, see <Link to="/blog">here</Link>.
           <br />
-          I'm currently working on a few personal
+          I'm currently working on a few personal{' '}
           <Link to="/projects">projects</Link>.
         </p>
 
         <p>
-          I think computers and programming are awesome and are one of my main
+          I think computers and programming are awesome and are one of my main{' '}
           <Link to="/interests">interests</Link>.
           <br />
           I love making and designing creative solutions to problems by thinking
@@ -207,29 +257,79 @@ function App() {
       </div>
     </div>
   )
+}
 
-  // return (
-  //   <>
-  //     <div classNameName="app">
-  //       <h1>Fullstack Boilerplate - with Fruits!</h1>
-  //       <ul>
-  //         {data &&
-  //           Object.entries(data.calendar).map(([key, value]) => {
-  //             return (
-  //               <li key={key}>
-  //                 {key}:{' '}
-  //                 {value.confirmed ? (
-  //                   <b>{value.count}</b>
-  //                 ) : (
-  //                   <i>{value.count}</i>
-  //                 )}
-  //               </li>
-  //             )
-  //           })}
-  //       </ul>
-  //     </div>
-  //   </>
-  // )
+function formatFullDate(date) {
+  return date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  })
+}
+
+function formatRelativeDate(estimate, start) {
+  const currentDate = start !== undefined ? new Date(start) : new Date()
+  const estimateDate = new Date(estimate)
+  const timeDiff = estimateDate - currentDate
+
+  let seconds = Math.floor(timeDiff / 1000)
+  let minutes = Math.floor(seconds / 60)
+  let hours = Math.floor(minutes / 60)
+  const days = Math.floor(hours / 24)
+  const years = Math.floor(days / 365)
+
+  if (years >= 10) {
+    // Just show the estimated year if a decade or so in the future
+    return estimateDate.getFullYear().toString()
+  } else if (years >= 2) {
+    // Just show the estimated month and year if a couple of years or so in the future
+    return estimateDate.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+    })
+  } else if (days >= 7) {
+    // Just show the month and day if a week or so in the future (plus the year to avoid confusion)
+    return estimateDate.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    })
+  } else if (days >= 1) {
+    // Just show the days and hours remaining if over a day in the future
+    hours %= 24
+    return `${days} ${days === 1 ? 'day' : 'days'}${
+      hours > 0 ? ` and ${hours} ${hours === 1 ? 'hour' : 'hours'}` : ''
+    }`
+  } else if (hours >= 1) {
+    // Just show the hours and minutes remaining if an hour or so in the future
+    minutes %= 60
+    return `${hours} ${hours === 1 ? 'hour' : 'hours'}${
+      minutes > 0
+        ? ` and ${minutes} ${minutes === 1 ? 'minute' : 'minutes'}`
+        : ''
+    }`
+  } else if (minutes >= 1) {
+    // Just show the minutes and seconds remaining if a minute or so in the future
+    seconds %= 60
+    return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}${
+      seconds > 0
+        ? ` and ${seconds} ${seconds === 1 ? 'second' : 'seconds'}`
+        : ''
+    }`
+  } else {
+    // Finally, all remaining estimates must be seconds away (negatives will go here, but none should be generated by this program)
+    return `${seconds} ${seconds === 1 ? 'second' : 'seconds'}`
+  }
+}
+
+function nextMilestone(level, count) {
+  const milestone = 100 * Math.pow(10, level)
+  const round = Math.ceil(count / milestone) * milestone
+
+  return round == count ? count + milestone : round
 }
 
 export default App
