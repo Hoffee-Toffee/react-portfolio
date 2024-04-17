81a17629794d360e9bfd50a692dc340496bce783
Hoffee-Toffee <Tristan-Bulmer@live.nmit.ac.nz>
2024-04-17 21:02:21 +1200

    Nav menu closes on scroll and navigation

--- client/hooks/useScroll.ts
+++ client/hooks/useScroll.ts
@@ -5,19 +5,25 @@ const useScroll = () => {
   const location = useLocation()
 
   useEffect(() => {
-    console.log(location.pathname)
+    // Close menu after navigating to a new page
+    document.getElementById('nav-toggle').checked = false
+
     const onInterests = location.pathname === '/interests'
     const scrollingBody = onInterests
       ? document.getElementById('interests')
       : window
 
     const handleScroll = () => {
+      // Close menu on scroll
+      document.getElementById('nav-toggle').checked = false
+
       // Header fade logic
       const header = document.querySelector('header')
       const headerHeight = header?.clientHeight || 0
       const scroll = onInterests ? scrollingBody.scrollTop : window.scrollY
       const header_opacity = 1 - scroll / headerHeight
-      header.style.opacity = Math.max(0, Math.min(header_opacity, 1)).toString()
+      header.style.opacity = Math.max(0, Math.min(header_opacity, 1))
+      header.style.pointerEvents = header.style.opacity < 0.5 ? 'none' : 'auto'
 
       // Footer fade logic
       const footer = document.querySelector('footer')
@@ -28,7 +34,8 @@ const useScroll = () => {
       const scrollPositionFromBottom =
         scrollHeight - (scroll + window.innerHeight)
       const footer_opacity = 1 - scrollPositionFromBottom / footerHeight
-      footer.style.opacity = Math.max(0, Math.min(footer_opacity, 1)).toString()
+      footer.style.opacity = Math.max(0, Math.min(footer_opacity, 1))
+      footer.style.pointerEvents = footer.style.opacity < 0.5 ? 'none' : 'auto'
     }
 
     scrollingBody.addEventListener('scroll', handleScroll)
