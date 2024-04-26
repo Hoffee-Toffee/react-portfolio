bf642b2f0e98565b926af61155f477c7562ead81
Hoffee-Toffee <Tristan-Bulmer@live.nmit.ac.nz>
2024-04-26 23:18:53 +1200

    Ping + build and submodules

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,12 @@
 import dotenv from 'dotenv'
-import server from './server.ts'
 dotenv.config()
+
+// If out of date version, kill this instance
+if (process.env.BUILD != 'Initial_Deployment') {
+  process.exit(0)
+}
+
+import server from './server.ts'
 
 const PORT = process.env.PORT || 3000
 
--- server/server.ts
+++ server/server.ts
@@ -9,17 +9,17 @@ import subRoutes from './routes/subs.ts'
 const server = express()
 
 server.use(express.json())
-// All folders of 'client/projects' should be accessible at the root e.g. 'client/projects/test' at simply 'test'
 
 server.use('/api/v1/sneezes', sneezeRoutes)
 server.use('/api/v1/projects', projectRoutes)
 server.use('/api/v1/secret', secretRoutes)
 server.use('/projects', subRoutes)
+server.get('/ping', (_, res) => res.json({ message: 'pong' }))
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
   server.use('/assets', express.static(Path.resolve('./dist/assets')))
-  server.get('*', (req, res) => {
+  server.get('*', (_, res) => {
     res.sendFile(Path.resolve('./dist/index.html'))
   })
 }
